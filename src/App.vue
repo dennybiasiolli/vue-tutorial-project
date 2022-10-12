06c334c5f7ffec860d35e29994e79977eed43cc6
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-10-12 16:38:48 +0200

    App: using example components

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
+<script setup>
+import OptionsAPI from './components/OptionsAPI.vue'
+import CompositionAPI from './components/CompositionAPI.vue'
+</script>
+
 <template>
   CIAO
+  <p>
+    <OptionsAPI />
+  </p>
+  <p>
+    <CompositionAPI />
+  </p>
 </template>
