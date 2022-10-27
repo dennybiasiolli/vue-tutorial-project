ee2532ba69bfbf7293602f521367f6dc5976dfc1
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-10-27 10:54:15 +0200

    day 2: final test page 1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import OptionsAPI from './components/OptionsAPI.vue'
 import CompositionAPI from './components/CompositionAPI.vue'
+import TodoUtility from './components/TodoUtility.vue'
 </script>
 
 <template>
@@ -10,5 +11,9 @@ import CompositionAPI from './components/CompositionAPI.vue'
   </p>
   <p>
     <CompositionAPI />
+  </p>
+  <hr />
+  <p>
+    <TodoUtility />
   </p>
 </template>
---
+++ src/components/TodoUtility.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const items = ref([])
+
+onMounted(() => {
+  items.value = [
+    { id: 1, text: 'Todo item 1', completed: false },
+    { id: 2, text: 'Todo item 2', completed: false },
+    { id: 3, text: 'Todo item 3', completed: true },
+    { id: 4, text: 'Todo item 4', completed: false },
+    { id: 5, text: 'Todo item 5', completed: true },
+  ];
+})
+</script>
+
+<template>
+  <h2>Todo Utility</h2>
+  <ul>
+    <li
+      v-for="(item, index) in items"
+      :key="item.id"
+      :class="{ 'item-completed': item.completed }"
+    >
+      {{ index + 1 }}. {{ item.text }}
+    </li>
+  </ul>
+</template>
+
+<style>
+.item-completed {
+  text-decoration: line-through;
+}
+</style>
