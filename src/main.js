5f487ba16e66e9d158e508f529ef74792a8b78d1
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-11-01 08:58:27 +0100

    declaring and using pinia

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import { router } from '@/router'
+import { pinia } from '@/stores'
 import App from './App.vue'
 
 const app = createApp(App)
+app.use(pinia)
 app.use(router)
 app.mount('#app')
