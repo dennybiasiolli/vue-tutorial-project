37e141154943dfcdd04c44e6619e60a6d477549c
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-11-02 09:14:32 +0100

    day 3: final test page 2.1

--- src/components/NavLinks.vue
+++ src/components/NavLinks.vue
@@ -2,16 +2,16 @@
   <nav>
     <ul>
       <li>
-        <router-link to="/">Home Page</router-link>
+        <router-link :to="{ name: 'home' }">Home Page</router-link>
       </li>
       <li>
-        <router-link to="/options-api">Options API</router-link>
+        <router-link :to="{ name: 'options-api' }">Options API</router-link>
       </li>
       <li>
-        <router-link to="/composition-api">Composition API</router-link>
+        <router-link :to="{ name: 'composition-api' }">Composition API</router-link>
       </li>
       <li>
-        <router-link to="/todo-utility">To-do Utility</router-link>
+        <router-link :to="{ name: 'todo-utility' }">To-do Utility</router-link>
       </li>
     </ul>
   </nav>
--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,26 @@ import OptionsAPI from '@/components/OptionsAPI.vue'
 import TodoUtility from '@/components/TodoUtility.vue'
 
 export const routes = [
-  { path: '/', component: HomePage },
-  { path: '/composition-api', component: CompositionAPI },
-  { path: '/options-api', component: OptionsAPI },
-  { path: '/todo-utility', component: TodoUtility },
+  {
+    path: '/',
+    name: 'home',
+    component: HomePage,
+  },
+  {
+    path: '/composition-api',
+    name: 'composition-api',
+    component: CompositionAPI,
+  },
+  {
+    path: '/options-api',
+    name: 'options-api',
+    component: OptionsAPI,
+  },
+  {
+    path: '/todo-utility',
+    name: 'todo-utility',
+    component: TodoUtility,
+  },
 ]
 
 export const router = createRouter({
